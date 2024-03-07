7f7e07f4a25ba066b45a92cef492d2831e2efa90
MelindaMob <melindamobiye0202@gmail.com>
2024-03-07 23:35:53 +0100

    ytb

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let slides = document.querySelectorAll('.imgCarroussel');
+let index = 0;
+
+function changeSlide() {
+    if(index < 0){
+        index = slides.length - 1;
+    }else if(index>=slides.length){
+        index=0;
+    }
+
+    slides.forEach( slide =>{
+        slide.classList.remove('active');
+    })
+
+    slides[index].classList.add('active');
+    index++;
+
+}
+setInterval(changeSlide, 4000);
+
+// let cardGuest = document.getElementById('cardGuest');
+// const container = document.getElementById('containtGuest');
+
+// function carousselAuto(){
+//     container.style.left = '50px';
+//     container.style.transition = '5s';
+// }
+// setInterval(carousselAuto, 5000);
